020dc234dc3ae111a189966489d03abfd6dc9df4
usuarioman <castilla.ar@gmail.com>
2023-08-07 22:12:29 -0600

    fix vietify.sh now is working as intended.

--- vitefy.sh
+++ vitefy.sh
@@ -5,34 +5,31 @@ fi
 
 appName="$2"
 
+# This first part is for the project creation with vite.
 # Create the app
 yarn create vite "$appName" --template react
-
 # Create enter to the new folder.
 cd $appName
-
-
 # Check if package.json exists
 if [ ! -f "package.json" ]; then
   echo "package.json does not exist. Exiting..."
   exit 1
 fi
-
 # Install dependencies
 yarn install
 
+
+# Once the project is up. Set jest configurations and boilerpate.
 # Check if src directory exists
 if [ ! -d "src" ]; then
   echo "src directory does not exist. Exiting..."
   exit 1
 fi
-
 # Create codeToTest directory within src
 mkdir -p "src/codeToTest"
-
 # Create a dummy code for testing.
 cat << EOF > src/codeToTest/dummyCode.js
-export const add = (a, b) => return a + b;
+export const add = (a, b) => a + b;
 EOF
 
 # Create test directory where the jest test resides.
@@ -48,20 +45,22 @@ describe('add', () => {
 });
 EOF
 
-# Install jest
+# Install jest as development dependency
 yarn add --dev jest
 
 # Configure script command to make a jest --watchAll
 jq '.scripts.test = "jest --watchAll"' package.json > tmp.json && mv -f tmp.json package.json
 
+# Type definitions. Yhe @types/jest module provide full typing when writing tests.
 yarn add -D @types/jest
 
+# Babel configurations.
 yarn add --dev babel-jest @babel/core @babel/preset-env
 
 cat << EOF > babel.config.cjs
-"module.exports = {
+module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-};"
+};
 EOF
 
 cwd=$(pwd)

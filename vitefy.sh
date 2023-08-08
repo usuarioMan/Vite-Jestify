dabae56dcd18f7b598ba9d40cf3d0c31f005fd39
usuarioman <castilla.ar@gmail.com>
2023-08-07 22:16:15 -0600

    add comments to vietify.sh

--- vitefy.sh
+++ vitefy.sh
@@ -1,38 +1,48 @@
+#!/bin/bash
+
+# Check if the number of arguments ($#) is less than 2 or if the first argument is not "--name"
 if [[ $# -lt 2 || "$1" != "--name" ]]; then
   echo "Usage: $0 --name <name_of_the_app>"
   exit 1
 fi
 
+# Store the second argument as the appName
 appName="$2"
 
-# This first part is for the project creation with vite.
-# Create the app
+# This first part is for creating a project using Vite.
+
+# Create the app using yarn create vite with the react template
 yarn create vite "$appName" --template react
-# Create enter to the new folder.
+# Enter the newly created folder
 cd $appName
+
 # Check if package.json exists
 if [ ! -f "package.json" ]; then
   echo "package.json does not exist. Exiting..."
   exit 1
 fi
-# Install dependencies
+
+# Install project dependencies
 yarn install
 
 
-# Once the project is up. Set jest configurations and boilerpate.
+# Once the project is set up, configure Jest and create boilerplate.
+
 # Check if src directory exists
 if [ ! -d "src" ]; then
   echo "src directory does not exist. Exiting..."
   exit 1
 fi
-# Create codeToTest directory within src
+
+# Create a directory for the code to be tested
 mkdir -p "src/codeToTest"
-# Create a dummy code for testing.
+
+# Create a dummy code file for testing
 cat << EOF > src/codeToTest/dummyCode.js
 export const add = (a, b) => a + b;
 EOF
 
-# Create test directory where the jest test resides.
+# Create a test directory for Jest tests
 mkdir test
 cat << EOF > test/dummyCode.test.js
 import { add } from '../src/codeToTest/dummyCode.js';
@@ -45,25 +55,29 @@ describe('add', () => {
 });
 EOF
 
-# Install jest as development dependency
+# Install Jest as a development dependency
 yarn add --dev jest
 
-# Configure script command to make a jest --watchAll
+# Configure the script command to run Jest in watch mode
 jq '.scripts.test = "jest --watchAll"' package.json > tmp.json && mv -f tmp.json package.json
 
-# Type definitions. Yhe @types/jest module provide full typing when writing tests.
+# Install type definitions for Jest to provide full typing when writing tests
 yarn add -D @types/jest
 
-# Babel configurations.
+# Configure Babel for Jest
 yarn add --dev babel-jest @babel/core @babel/preset-env
 
+# Create Babel configuration
 cat << EOF > babel.config.cjs
 module.exports = {
   presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
 };
 EOF
 
+# Get the current working directory
 cwd=$(pwd)
+
+# Use AppleScript to open two Terminal windows
 osascript <<EOD
 tell application "Terminal"
     activate
